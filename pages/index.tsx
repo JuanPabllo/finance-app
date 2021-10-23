93d03c789e689748ad62e91fcbc32998ab851f9b
juanpabllo <juanpablo192.dreaw@gmail.com>
2021-10-23 15:18:15 -0300

    test button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
 
+import Button from './components/Button';
+
 const Home: NextPage = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button loading={true}>Botão</Button>
+    </>
+  );
 };
 
 export default Home;
